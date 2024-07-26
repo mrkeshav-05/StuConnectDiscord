8cdf835af1c38dbeccc6a882ef227eaa1d362a40
Jovial-Kanwadia <143182327+Jovial-Kanwadia@users.noreply.github.com>
2024-07-26 22:28:32 +0530

    Update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -21,7 +21,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 app.use(cors({
-    origin: "*",
+    origin: "https://stuconnect-app.vercel.app",
     credentials: true,
 }));
 
